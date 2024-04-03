f1a64560afb64243eed6ae64b58163c46a8c7b70
mike seibel <mikes@washington.edu>
2024-04-02 17:49:08 -0700

    basic_list thru uw-list-manager api

--- uw_mailman/basic_list.py
+++ uw_mailman/basic_list.py
@@ -6,40 +6,37 @@ Interface for interacting with the mailman uwnetid resource
 """
 
 import json
-from commonconf import settings
 from uw_mailman import get_resource
 
 
-URL = "/{key}/admin/v1.0/uwnetid/available/?uwnetid={uwnetid}"
-MAILMAN_ADMIN_URL = "https://mailman.u.washington.edu/mailman/admin/{}"
+URL = "/uw_list_manager/api/v1/list/{list_name}/exists"
 
 
 def _get_url_path(list_name):
     """
-    Live Dao requires RESTCLIENTS_MAILMAN_KEY in the settings.py
+    existance test url for given list name
     """
-    access_key = getattr(settings,
-                         "RESTCLIENTS_MAILMAN_KEY",
-                         "__mock_key__")
-    return URL.format(key=access_key, uwnetid=list_name)
+    return URL.format(list_name=list_name)
+
+
+def get_list_by_name(list_name):
+    """
+    Return the mailman list object for the given list name string
+    @param list_name: a non_empty string
+    """
+    return json.loads(get_resource(_get_url_path(list_name)))
 
 
 def exists(list_name):
     """
-    Return True if the corresponding mailman list is avaliable
+    Return True if the corresponding mailman list already exists
     for the given list name string
     @param list_name: a non_empty string
     """
-    return _process_json(get_resource(_get_url_path(list_name)))
-
-
-def _process_json(response_body):
-    """
-    Returns True the list already exists
-    """
-    data = json.loads(response_body)
-    return "Available" in data and data["Available"] == "False"
+    mlist = get_list_by_name(list_name)
+    return mlist.get('exists', False)
 
 
 def get_admin_url(list_name):
-    return MAILMAN_ADMIN_URL.format(list_name)
+    mlist = get_list_by_name(list_name)
+    return mlist.get("admin_url", "")
--- uw_mailman/dao.py
+++ uw_mailman/dao.py
@@ -1,7 +1,6 @@
 # Copyright 2024 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
-import logging
 import os
 from os.path import abspath, dirname
 from restclients_core.dao import DAO
@@ -13,3 +12,7 @@ class Mailman_DAO(DAO):
 
     def service_mock_paths(self):
         return [abspath(os.path.join(dirname(__file__), "resources"))]
+
+    def _custom_headers(self, method, url, headers, body):
+        api_token = self.get_service_setting('API_TOKEN', '')
+        return {'Authorization': 'Token {}'.format(api_token)}
